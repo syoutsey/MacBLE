60cea9c20806cb466b35081640497d410ef07c04
Sam Youtsey <sam.youtsey@gmail.com>
2014-09-08 16:25:22 -0700

    Initial commit

--- MacBLE/AppDelegate.swift
+++ MacBLE/AppDelegate.swift
@@ -1,26 +1,44 @@
-//
-//  AppDelegate.swift
-//  MacBLE
-//
-//  Created by Sam Youtsey on 9/8/14.
-//  Copyright (c) 2014 Nebari. All rights reserved.
-//
-
 import Cocoa
+import IOBluetooth
 
-class AppDelegate: NSObject, NSApplicationDelegate {
-                            
+class AppDelegate: NSObject, NSApplicationDelegate, CBCentralManagerDelegate {
+    
     @IBOutlet weak var window: NSWindow!
-
-
+    var centralManager: CBCentralManager?
+    var scanning = false
+    
     func applicationDidFinishLaunching(aNotification: NSNotification?) {
-        // Insert code here to initialize your application
+        centralManager = CBCentralManager(delegate: self, queue: nil)
     }
-
-    func applicationWillTerminate(aNotification: NSNotification?) {
-        // Insert code here to tear down your application
+    
+    func centralManagerDidUpdateState(central: CBCentralManager!) {
+        if central.state == CBCentralManagerState.PoweredOn && !scanning {
+            centralManager!.scanForPeripheralsWithServices(nil, options: nil)
+            println("Scanning")
+            scanning = true
+        }
     }
+    
+    func centralManager(central: CBCentralManager!, didDiscoverPeripheral peripheral: CBPeripheral!, advertisementData: [NSObject : AnyObject]!, RSSI: NSNumber!) {
+        var proximityUUID: String?
+        if let data = advertisementData[CBAdvertisementDataManufacturerDataKey] as? NSData {
+            println("Data size: \(data.length) bytes")
+            proximityUUID = parseBeaconAdvertisement(data)
+        }
+        println("Found Beacon: \(peripheral.name), proximityUUID: \(proximityUUID), uuidstring: \(peripheral.identifier.UUIDString)")
+    }
+    
+    func parseBeaconAdvertisement(data: NSData) -> String {
+        if data.length != 25 {
+            return "null"
+        }
 
-
+        var uuidBytes = [Character](count: 17, repeatedValue: "0")
+        var uuidRange = NSMakeRange(4, 16)
+        data.getBytes(&uuidBytes, range: uuidRange)
+        var bytes = UnsafeMutablePointer<UInt8>(uuidBytes)
+        var proximityUUID = NSUUID(UUIDBytes: bytes)
+        return proximityUUID.UUIDString
+    }
 }
 
