e0f00f3626fe42132b327ebc1f885797cbeff66f
Sam Youtsey <sam.youtsey@gmail.com>
2014-09-08 13:55:37 -0700

    Initial Commit

---
+++ MacBLE/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  MacBLE
+//
+//  Created by Sam Youtsey on 9/8/14.
+//  Copyright (c) 2014 Nebari. All rights reserved.
+//
+
+import Cocoa
+
+class AppDelegate: NSObject, NSApplicationDelegate {
+                            
+    @IBOutlet weak var window: NSWindow!
+
+
+    func applicationDidFinishLaunching(aNotification: NSNotification?) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(aNotification: NSNotification?) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ MacBLE/main.swift
@@ -0,0 +1,11 @@
+//
+//  main.swift
+//  MacBLE
+//
+//  Created by Sam Youtsey on 9/8/14.
+//  Copyright (c) 2014 Nebari. All rights reserved.
+//
+
+import Cocoa
+
+NSApplicationMain(C_ARGC, C_ARGV)
